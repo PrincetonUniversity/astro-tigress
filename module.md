29b68b3f06b567b7efed3cdf22576065179eac76
Chang-Goo Kim <cgkim@astro.princeton.edu>
2022-06-03 16:27:23 -0400

    Revised doc strings and added API reference docs

--- module.md
+++
@@ -1,5 +0,0 @@
-# Code reference
-::: module.tigress_read.Model
-    options:
-      show_root_heading: false
-      show_source: false
--- module/tigress_read.py
+++ module/tigress_read.py
@@ -6,11 +6,15 @@ import ytathena as ya
 import radmc
 
 class Model:
-    """Class containing the simulation model information."""
+    """Class containing the simulation model information.
+
+    Parameters
+    ----------
+    model_id: str
+        name of the model, e.g. `R8_2pc`.
+    """
+
     def __init__(self, dir_master, model_id):
-        """Initialization.
-        input:
-            model_id: string, name of the model, e.g. "R8_2pc"."""
         self.dir_master = dir_master
         self.model_id = model_id
         self.dir_model = dir_master + model_id + "/"
@@ -19,7 +23,7 @@ class Model:
         ivtks.sort()
         self.ivtks = np.array(ivtks) #list of vtk numbers of outputs
         #contained data sets, e.g. ["MHD", "chem", "CO_lines"]
-        self.data_sets = {} 
+        self.data_sets = {}
         for ivtk in self.ivtks:
             dir_ivtk = "{:s}{:04d}/".format(self.dir_model, ivtk)
             self.data_sets[ivtk] = os.listdir(dir_ivtk)
@@ -34,25 +38,31 @@ class Model:
         if os.path.isfile(fn_input_MHD):
             self.MHD_input = athena_read.athinput(fn_input_MHD)
             self.fn_input_MHD = fn_input_MHD
-        for k in self.MHD_input.keys(): 
-            if k.startswith("output"): 
+        for k in self.MHD_input.keys():
+            if k.startswith("output"):
                 if self.MHD_input[k]["out_fmt"] == "vtk":
                     self.dt_code = self.MHD_input[k]["dt"]
                     self.dt_myr = self.dt_code * const.pc/const.km/const.myr
                     self.t_myr = self.ivtks * self.dt_myr
         self.CO_lines = {}
         return
+
     def load(self, ivtk, dataset="MHD", Z=1., iline=1, Tdect=0.):
         """Load simulation data from a certain data set.
-        input:
-            ivtk: int, vtk output number
-            dataset: string, name of the dataset. {"MHD", "chem", "CO_lines"}
-            Z: float, metallicity for chemistry post-processing. {0.5, 1, 2}. 
-               Default: Z = 1
-            iline: int, uppper level of CO rotational line {1: J=1-0, 2: J=2-1}
-                   Default: iline = 1
-            Tdect: float, detection limit for atenna temperature in K
-                   for each velocity channel. Default: Tdect = 0.
+
+        Parameters
+        ----------
+        ivtk: int
+            vtk output number
+        dataset: srt, ["MHD", "chem", "CO_lines"]
+            name of the dataset.
+        Z: float, [0.5, 1, 2]
+            metallicity for chemistry post-processing.
+        iline: int, [1, 2]
+            uppper level of CO rotational line. 1 for J=1-0, 2 for J=2-1
+        Tdect: float
+            detection limit for atenna temperature in K
+            for each velocity channel.
         """
         dir_ivtk = "{:s}{:04d}/".format(self.dir_model, ivtk)
         dict_Z = {0.5:"Z05", 1.:"Z1", 2.:"Z2"}
@@ -76,34 +86,48 @@ class Model:
         return
 
 class DataMHD:
-    """MHD data in one time snapshot."""
+    """MHD data in one time snapshot.
+
+    Parameters
+    ----------
+    fn: str
+        filename.
+    """
     def __init__(self, fn):
-        """Initialization.
-        input:
-            fn: string, filename"""
         self.fn = fn
         self.ytds = ya.load_athena4p2(fn) #yt data
         self.grid = ya.get_covering_grid(self.ytds) #yt covering grid
         return
 
 class DataChem:
-    """Chemistry data in one time snapshot."""
+    """Chemistry data in one time snapshot.
+
+    Parameters
+    ----------
+    fn: str
+        filename.
+    """
     def __init__(self, fn):
-        """Initialization.
-        input:
-            fn: string, filename"""
         self.fn = fn
         self.ytds = ya.load_athenapp(fn) #yt data
         self.grid = ya.get_covering_grid(self.ytds) #yt covering grid
         return
     def get_col(self, spec, axis=2):
         """Calculate the column density of species.
-        input:
-            spec: string, name of species, e.g. "H2".
-            axis: int, axis where the column is calculated along. 
-                  0:x, 1:y, 2:z.
-        return:
-            col: YTarray, column density, e.g. N_H2."""
+
+        Parameters
+        ----------
+        spec: str
+            name of species, e.g. "H2".
+        axis: int, [0, 1, 2]
+            axis where the column is calculated along.
+            0:x, 1:y, 2:z.
+
+        Returns
+        -------
+        col: YTarray
+            column density, e.g. N_H2.
+        """
         dx = self.grid["dx"][0, 0, 0]
         nH = self.grid["nH"]
         abd = self.grid[spec]
@@ -112,15 +136,19 @@ class DataChem:
         return col
 
 class DataCO:
-    """CO emission lines PPV in one time snapshot."""
+    """CO emission lines PPV in one time snapshot.
+
+    Parameters
+    ----------
+    fn: str
+        filename
+    iline: int, [1, 2]
+        index for CO rotaional line upper level.
+    Tdect: float
+        detection limit for atenna temperature in K
+        for each velocity channel.
+    """
     def __init__(self, fn, iline, Tdect=0.):
-        """Initialization.
-        input:
-            fn: string, filename
-            iline: int, index for CO rotaional line upper level. 
-                   {1: J=1-0, 2: J=2-1}.
-            Tdect: float, detection limit for atenna temperature in K
-                   for each velocity channel. Default is 0."""
         if iline == 1:
             self.lam0 = 2600.75763346
         elif iline == 2:
@@ -131,7 +159,7 @@ class DataCO:
                     iline)
             raise RuntimeError(msg)
         self.fn = fn
-        self.Tdect = Tdect 
+        self.Tdect = Tdect
         self.img = radmc.Images(fn, lam0=self.lam0)  #PPV cube
         self.WCO = self.img.getimageWCO(Tdect=Tdect) #CO line intensity in K*km/s
         self.Tpeak = self.img.getTpeak(Tdect=Tdect)  #peak atenna temperature in K
